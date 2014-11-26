a72c06f498c9fab7ac7fcac3d427b744eec9f17e
CAvancouver <mimicat666@126.com>
2014-11-26 16:21:02 +0800

    pass

--- testepoll.py
+++ testepoll.py
@@ -5,7 +5,7 @@ EOL1=b'\n\n'
 EOL2=b'\n\r\n'
 response =b'HTTP/1.0 200 OK\r\nDate:Mon, 1 Jan 1996 01:01:01 GMT\r\n'
 response +=b'Content-Type: text/plain\r\nContent-Length: 13\r\n\r\n'
-response +=b'hello,world!'
+response +=b'hello,epoll world!'
 
 serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -14,38 +14,42 @@ serversocket.listen(1)
 serversocket.setblocking(0)
 
 epoll=select.epoll()
-epoll.regiest(serversocket.fileno(),select.EPOLLIN)
+epoll.register(serversocket.fileno(),select.EPOLLIN)
 
 try:
-	connections={};requests={};responses{}
+	connections={};requests={};responses={}
 	while True:
 		events=epoll.poll(1)
-		for fileo,event in events:
+		for fileno,event in events:
 			if fileno==serversocket.fileno():
-				connection,address=seversocker.accept()
+				print fileno,event,events
+				connection,address=serversocket.accept()
 				connection.setblocking(0)
 				epoll.register(connection.fileno(),select.EPOLLIN)
 				connections[connection.fileno()]=connection
 				requests[connection.fileno()]=b''
-				responses[connection.fileno()]=rqsponse
+				responses[connection.fileno()]=response
 			elif event & select.EPOLLIN:
+				print fileno,event,select.EPOLLIN,events
 				requests[fileno]+=connections[fileno].recv(1024)
 				if EOL1 in requests[fileno] or EOL2 in requests[fileno]:
 					epoll.modify(fileno,select.EPOLLOUT)
-					print('-'*40+'\n'+requests[filno].decode()[:-2])
+					print('-'*40+'\n'+requests[fileno].decode()[:-2])
 			elif event & select.EPOLLOUT:
+				print fileno,event,select.EPOLLOUT,events
 				byteswritten=connections[fileno].send(responses[fileno])
 				responses[fileno]=responses[fileno][byteswritten:]
-				if len(responses[fileno])==0
+				if len(responses[fileno])==0:
 					epoll.modify(fileno,0)
 					connections[fileno].shutdown(socket.SHUT_RDWR)
 			elif event & select.EPOLLHUP:
+				print fileno,event,select.EPOLLHUP,events
 				epoll.unregister(fileno)
 				connections[fileno].close()
 				del connections[fileno]
 
 finally:
-	epoll.unregister().close()
+	epoll.unregister(serversocket.fileno())
 	epoll.close()
 	serversocket.close()
 
